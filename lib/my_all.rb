ccf06476328536370dcd78336718b2fde27fb399
George Dilthey <george.dilthey@gmail.com>
2021-02-06 23:33:25 -0500

    commit tree 584bce15100694213f5cba15d62507832c45e21b

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+    i = 0
+    final_array = []
+    while i < collection.length
+        final_array << yield(collection[i])
+        i += 1
+    end
 
+    if final_array.include?(false)
+        return false
+    else
+        return true
+    end
 end
